73b283683e8963c642762161cac163d1b5688548
ameliaritger <47021807+ameliaritger@users.noreply.github.com>
2021-06-02 15:57:16 -0700

    fix filename from MARINe data

--- marine-subset.R
+++ marine-subset.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(janitor)
 
 #load data
-marine <- read_csv("amelia_ritger_cbs_summary_2020_1005.csv") %>% 
+marine <- read_csv("amelia_ritger_cbs_summary_2020_1005-quad.csv") %>% 
   clean_names()
 
 #subset for san diego
