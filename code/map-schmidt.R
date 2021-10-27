abd02e72ac4d2206184796df1425588481b4cc0b
ameliaritger <47021807+ameliaritger@users.noreply.github.com>
2021-10-27 11:43:35 -0700

    not really working, starting expressions?

--- code/map-schmidt.R
+++ code/map-schmidt.R
@@ -22,34 +22,19 @@ ggplot() +
   xlim(-127,-114) +
   xlab("Longitude") +
   ylab("Latitude") +
-  labs(caption=str_wrap("Figure 1. Map of study sites for Aim 2. Blue circles represent areas where community surveys were conducted. Red triangles represent areas where sensors were deployed and surveys were conducted.", 60)) +
+  labs(caption=str_wrap(expression(paste(bold("Figure 1."),"Figure 1. Map of study sites for Aim 2. Blue circles represent areas where community surveys were conducted. Red triangles represent areas where sensors were deployed and surveys were conducted.", 55)))) +
   geom_point(data=sites, aes(x=long, y=lat, color=name, shape=shape), size=4.5) +
   geom_text(data=sites, aes(x=long, y=lat, label=name), size=6,
             hjust=ifelse(sites$name=='ALG',1,1.25), 
             vjust=ifelse(sites$name=='ALG',1.5,0)) +
-  annotate(geom = "text", #add a caption
-           label = "Figure 1. Map of study sites for Aim 2. Blue circles represent areas", 
-           x = -120, y = 35, #orient this to figure coordinates
-           hjust = 0.55, vjust = 18, #remove it from figure to margin
-           size = 5) +
-  annotate(geom = "text", #add a caption
-           label = "where community surveys were conducted. Red triangles represent", 
-           x = -120, y = 35, #orient this to figure coordinates
-           hjust = 0.54, vjust = 19.5, #remove it from figure to margin
-           size = 5) +
-  annotate(geom = "text", #add a caption
-           label = "areas where sensors were deployed and surveys were conducted.", 
-           x = -120, y = 35, #orient this to figure coordinates
-           hjust = 0.5, vjust = 21, #remove it from figure to margin
-           size = 5) +
   scale_color_manual(values = c("orangered3", "orangered3", "royalblue4", "royalblue4", "orangered3","royalblue4")) +
-  coord_cartesian(clip="off") + #remove coordinates to allow increasing margin for caption
+  #coord_cartesian(clip="off") + #remove coordinates to allow increasing margin for caption
   theme_minimal() +
   theme(legend.position="none",
-        plot.margin=unit(c(0,1.5,4,0.5),"cm"), #create lower margin for caption
+        #plot.margin=unit(c(0,0,0,0),"cm"), #create lower margin for caption
         axis.title=element_text(size=20),
         axis.text=element_text(size=15),
-        plot.caption = element_text(hjust=0.5, size=10))
+        plot.caption = element_text(hjust=0, size=14))
 
 
 ggsave("figures/fig1.png", width=6, height=10)
