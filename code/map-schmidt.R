7c9e01d0c590387fdba50be82dbab3bde8af2fff
ameliaritger <47021807+ameliaritger@users.noreply.github.com>
2021-10-27 11:47:27 -0700

    bold caption appropriately

--- code/map-schmidt.R
+++ code/map-schmidt.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(ggmap)
 library(maps)
 library(mapdata)
+library(ggtext) #for captions
 
 states <- map_data("state")
 west_coast <- subset(states, region %in% c("california", "oregon", "washington"))
@@ -22,7 +23,7 @@ ggplot() +
   xlim(-127,-114) +
   xlab("Longitude") +
   ylab("Latitude") +
-  labs(caption=str_wrap(expression(paste(bold("Figure 1."),"Figure 1. Map of study sites for Aim 2. Blue circles represent areas where community surveys were conducted. Red triangles represent areas where sensors were deployed and surveys were conducted.", 55)))) +
+  labs(caption="<b>Figure 1.</b> Map of study sites for Aim 2. Blue circles represent areas where community surveys were conducted. Red triangles represent areas where both oceanographic sensors were deployed and community surveys were conducted.") +
   geom_point(data=sites, aes(x=long, y=lat, color=name, shape=shape), size=4.5) +
   geom_text(data=sites, aes(x=long, y=lat, label=name), size=6,
             hjust=ifelse(sites$name=='ALG',1,1.25), 
@@ -31,11 +32,10 @@ ggplot() +
   #coord_cartesian(clip="off") + #remove coordinates to allow increasing margin for caption
   theme_minimal() +
   theme(legend.position="none",
-        #plot.margin=unit(c(0,0,0,0),"cm"), #create lower margin for caption
+        plot.margin=unit(c(0,0,1,0),"cm"), #create lower margin for caption to fit
         axis.title=element_text(size=20),
         axis.text=element_text(size=15),
-        plot.caption = element_text(hjust=0, size=14))
-
+        plot.caption=element_textbox_simple(hjust=0, vjust=1.5, size=14)) #allow caption to wrap
+        #plot.caption = element_text(hjust=0, size=14))
 
 ggsave("figures/fig1.png", width=6, height=10)
-
