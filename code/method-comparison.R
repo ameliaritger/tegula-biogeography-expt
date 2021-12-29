f47081ee014b1af39e7411cf4df8804d519778fc
ameliaritger <47021807+ameliaritger@users.noreply.github.com>
2021-12-28 19:59:54 -0800

    incorporate data from re-analyzing some of the alegria data

--- code/method-comparison.R
+++ code/method-comparison.R
@@ -1,8 +1,13 @@
 library(tidyverse)
 library(here)
+library(lubridate)
 
-compare <-read_csv(here("data","sensors", "method_comparison.csv"))
-
+compare <-read_csv(here("data","sensors", "method_comparison2.csv")) %>%
+  unite("date_time", "date", "time", sep="\ ") %>%
+  mutate(date_time=mdy_hms(date_time)) %>% #apply lubridate to date/time column
+  select(site, sensor_number, type, date_time, temp_c, p_h) %>%
+  mutate(type=ifelse(type=="man2", "manual-reanalysis", type))
+  
 alg <- compare %>%
   filter(site=="Alegria")
 
@@ -11,6 +16,11 @@ bml <- compare %>%
 
 lol <- compare %>%
   filter(site=="Lompoc Landing")
+
+ggplot(alg, aes(x=date_time, y=p_h, group=type)) +
+  geom_line(aes(color=type), size=1, alpha=0.5)
+
+ggsave(here("figures",  "method_comparison_ALG.png"), height=20, width=40, units="cm")
 
 ggplot(compare, aes(x=date_time, y=p_h, group=type)) +
   geom_line(aes(color=type), size=1, alpha=0.5) +
